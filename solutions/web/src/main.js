ffd7e4523781d0b0257c44d677664ed4ec4d7ffc
Cayan Vieira <filipecayan@gmail.com>
2023-06-15 15:39:19 -0300

    adicionando apollo ao projeto

--- solutions/web/src/main.js
+++ solutions/web/src/main.js
@@ -2,13 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router/route'
+import { createProvider } from './vue-apollo'
+import VueApollo from 'vue-apollo'
+import 'regenerator-runtime/runtime'
 
 
 Vue.config.productionTip = false
 Vue.use(router)
+Vue.use(VueApollo)
 
 new Vue({
   vuetify,
-  router,  
-  render: (h) => h(App),
+  router,
+  apolloProvider: createProvider(),
+  render: (h) => h(App)
 }).$mount('#app');
