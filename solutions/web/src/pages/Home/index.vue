d2f62e8561a54161d96fff2a335500d08b0571da
Cayan Vieira <filipecayan@gmail.com>
2023-06-23 12:02:29 -0300

    alteração - layout de acordo com os dados do banco

--- solutions/web/src/pages/Home/index.vue
+++ solutions/web/src/pages/Home/index.vue
@@ -1,127 +1,127 @@
-<template>    
-      <div class="black" >        
-        <v-container>
-          <v-text-field
-            hide-details
-            outlined
-            prepend-inner-icon="mdi-magnify"
-            label="pesquisar"
-            class="white ma-5"
-          >
-            Pesquisar
-          </v-text-field>
-          <div class="white--text"></div>
-          <v-card v-for="(genre, i) in genres" :key="i" class="white ma-5" >
-            <v-card-title>
-              {{ genre }}
-            </v-card-title>
-            <v-divider></v-divider>
-            <v-card-actions>
-              <v-slide-group show-arrows>
-                <v-slide-item
-                  v-for="(movie, i) in moviesByGenre[genre]"
-                  :key="i"
-                  @click="() => true"
-                >
-                  <v-card
-                    @click="detail = movie"
+<template>
+  <div class="black">
+    <v-container>
+      <v-text-field
+        hide-details
+        outlined
+        prepend-inner-icon="mdi-magnify"
+        label="pesquisar"
+        class="white ma-5"
+      >
+        Pesquisar
+      </v-text-field>
+      <div class="white--text"></div>
+      <v-card v-for="(genre, i) in genres" :key="i" class="white ma-5">  
+        <v-card-title>
+          {{ genre.name }}
+        </v-card-title>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-slide-group show-arrows>
+            <v-slide-item
+              v-for="(movie, i) in moviesByGenre[genre.name]"
+              :key="i"
+              @click="() => true"
+            >
+              <v-card
+                @click="detail = movie"
+                :key="i"
+                class="grey mx-2 pa-1"
+                width="20vw"
+                min-width="256px"
+              >
+                <v-card-title class="d-flex justify-center text-h6">
+                  {{ movie.title }}
+                </v-card-title>
+                <v-divider></v-divider>
+                <div class="d-flex justify-center" width="200px" height="100px">
+                  <v-img
+                    v-if="!movie.img"
+                    class="mb-5"
+                    src="../../assets/img/movie.png"
+                    max-height="200px"
+                    max-width="200px"
+                  />
+                </div>
+                <v-card-actions class="pa-3 d-flex justify-center">
+                  <v-btn
+                    fab
+                    small
+                    v-for="(avaliation, i) in userMovieAvaliation"
                     :key="i"
-                    class="grey mx-2 pa-1"
-                    width="20vw"
-                    min-width="256px"
+                    ><v-icon>{{ avaliation.icon }}</v-icon></v-btn
                   >
-                    <v-card-title class="d-flex justify-center text-h6">
-                      {{ movie.name }}
-                    </v-card-title>
-                    <v-divider></v-divider>
-                    <div
-                      class="d-flex justify-center"
-                      width="200px"
-                      height="100px"
-                    >
-                      <v-img
-                        v-if="!movie.img"
-                        class="mb-5"
-                        src="../../assets/img/movie.png"
-                        max-height="200px"
-                        max-width="200px"
-                      />
-                    </div>
-                    <v-card-actions class="pa-3 d-flex justify-center">
-                      <v-btn
-                        fab
-                        small
-                        v-for="(avaliation,i) in userMovieAvaliation"
-                        :key="i"
-                        ><v-icon>{{avaliation.icon}}</v-icon></v-btn
-                      >
-                    </v-card-actions>
-                  </v-card>
-                </v-slide-item>
-              </v-slide-group>
-            </v-card-actions>
-          </v-card>
-        </v-container>
-      </div>    
-  </template>
+                </v-card-actions>
+              </v-card>
+            </v-slide-item>
+          </v-slide-group>
+        </v-card-actions>
+      </v-card>
+    </v-container>
+  </div>
+</template>
   
   <script>
-  export default {
-    name: "Home-page",
-    data: () => ({
-      detail: null,
-      genres: ["Drama", "Comédia", "Ação", "Terror"],
-      userMovieAvaliation: [
-        { icon: "mdi-star", value: 1 },
-        { icon: " mdi-star", value: 2 },
-        { icon: "mdi-star", value: 3 },
-        { icon: "mdi-star", value: 4 },
-        { icon: "mdi-star", value: 5 },
-      ],
-      movies: [
-        { name: "Drama", genre: "Drama" },
-        { name: "Terror", genre: "Terror" },
-        { name: "Comédia ", genre: "Comédia" },
-        { name: "Ação", genre: "Ação" },
-        { name: "Drama", genre: "Drama" },
-        { name: "Drama", genre: "Drama" },
-        { name: "Terror", genre: "Terror" },
-        { name: "Comédia ", genre: "Comédia" },
-        { name: "Ação", genre: "Ação" },
-        { name: "Drama", genre: "Drama" },
-        { name: "Drama", genre: "Drama" },
-        { name: "Terror", genre: "Terror" },
-        { name: "Comédia ", genre: "Comédia" },
-        { name: "Ação", genre: "Ação" },
-        { name: "Drama", genre: "Drama" },
-        { name: "Drama", genre: "Drama" },
-        { name: "Terror", genre: "Terror" },
-        { name: "Comédia ", genre: "Comédia" },
-        { name: "Ação", genre: "Ação" },
-        // { name: "Drama", genre: "Drama" },
-      ],
-      moviesByGenre: {},
-    }),
-    created() {
-      this.agroupByGenre();
+export default {
+  name: "Home-page",
+  data: () => ({
+    detail: null,
+    genres: null,
+    userMovieAvaliation: [
+      { icon: "mdi-star", value: 1 },
+      { icon: " mdi-star", value: 2 },
+      { icon: "mdi-star", value: 3 },
+      { icon: "mdi-star", value: 4 },
+      { icon: "mdi-star", value: 5 },
+    ],
+    movies: null,
+    moviesByGenre: {},
+  }),
+  watch: {},
+  created() {
+    this.init();
+  },
+  mounted() {},
+  computed: {},
+  methods: {
+    async init() {
+      await this.fecthGenres();
+      await this.fecthMovies();
+      await this.agroupByGenre();
     },
-    computed: {},   
-    methods: {
-      agroupByGenre() {
-        let result = {};
-        this.genres.forEach((genre) => {
-          Object.defineProperty(result, genre, {
-            value: this.movies.filter((movie) => movie.genre === genre),
+    agroupByGenre() {
+      let result = {};
+      
+      this.genres.forEach((genre) => {
+        Object.defineProperty(result, genre.name, {
+            value: this.movies.filter((movie) => {             
+              return movie.in_genre[0].name === genre.name
+            })
           });
-        });
-  
-        this.moviesByGenre = result;
-  
-        return this.moviesByGenre;
-      },
+        });        
+      this.moviesByGenre = result;
+        
+      return this.moviesByGenre;
     },
-  };
-  </script>
+    async fecthMovies() {
+
+      const fetchMovies = await this.$apollo.query({
+        query: require(`@/graphql/fetchMovies.gql`),
+      });
+
+      return this.movies = fetchMovies.data.Movie;
+    },
+    async fecthGenres() {
+
+      const fetchGenres = await this.$apollo.query({
+        query: require(`@/graphql/fetchGenres.gql`),
+      });
+
+      this.genres = fetchGenres.data.Genre;
+    },
+  },
+};
+</script>
   <!-- exemplo estrutura de dados de um filme 
   {
     name:........,
@@ -133,4 +133,4 @@
   
   -->
   <style>
-  </style>
+</style>
