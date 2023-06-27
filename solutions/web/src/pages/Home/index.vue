7ffca97297b840f5b36890604016a433eae2efe5
Cayan Vieira <filipecayan@gmail.com>
2023-06-27 17:50:27 -0300

    ajustes no front

--- solutions/web/src/pages/Home/index.vue
+++ solutions/web/src/pages/Home/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="black">
-    <v-container>
+    <v-container class="bodyWidth">
       <v-text-field
         hide-details
         outlined
@@ -10,8 +10,51 @@
       >
         Pesquisar
       </v-text-field>
-      <div class="white--text"></div>
-      <v-card v-for="(genre, i) in genres" :key="i" class="white ma-5">  
+      <v-card v-if='recommendedMovies[0]' class="white ma-5">
+        <v-card-title> Recomendados </v-card-title>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-slide-group show-arrows>
+            <v-slide-item v-for="(item, i) in recommendedMovies" :key="i">
+              <v-card
+                :key="i"
+                class="grey mx-2 pa-1"
+                width="20vw"
+                min-width="256px"
+                max-width="300px"
+              >
+                <v-card-title class="d-flex justify-center text-h6">
+                  {{ item.Movie.title }}
+                </v-card-title>
+                <v-divider></v-divider>
+                <div class="d-flex justify-center" v-if="!item.Movie.poster">
+                  <v-img
+                    class="mb-5"
+                    src="../../assets/img/movie.png"                   
+                  />
+                </div>
+                <div class="d-flex justify-center" v-if="item.Movie.poster">
+                  <v-img
+                    class="mb-5"
+                    :src="item.Movie.poster"                    
+                  />
+                </div>
+                <v-card-actions class="pa-3 d-flex justify-center">
+                  <v-btn
+                    fab
+                    small
+                    @click="ratedMovie(item.Movie.movieId, rated.value)"
+                    v-for="(rated, i) in userMovieRated"
+                    :key="i"
+                    ><v-icon>{{ rated.icon }}</v-icon></v-btn
+                  >
+                </v-card-actions>
+              </v-card>
+            </v-slide-item>
+          </v-slide-group>
+        </v-card-actions>
+      </v-card>
+      <v-card v-for="(genre, i) in genres" :key="i" class="white ma-5">
         <v-card-title>
           {{ genre.name }}
         </v-card-title>
@@ -24,32 +67,30 @@
               @click="() => true"
             >
               <v-card
-                @click="detail = movie"
                 :key="i"
                 class="grey mx-2 pa-1"
                 width="20vw"
                 min-width="256px"
+                max-width="300px"
               >
                 <v-card-title class="d-flex justify-center text-h6">
                   {{ movie.title }}
                 </v-card-title>
                 <v-divider></v-divider>
-                <div class="d-flex justify-center" width="200px" height="100px">
-                  <v-img
-                    v-if="!movie.img"
-                    class="mb-5"
-                    src="../../assets/img/movie.png"
-                    max-height="200px"
-                    max-width="200px"
-                  />
+                <div class="d-flex justify-center" v-if="!movie.poster">
+                  <v-img class="mb-5" src="../../assets/img/movie.png" />
+                </div>
+                <div class="d-flex justify-center" v-if="movie.poster">
+                  <v-img class="mb-5" :src="movie.poster" />
                 </div>
                 <v-card-actions class="pa-3 d-flex justify-center">
                   <v-btn
                     fab
                     small
-                    v-for="(avaliation, i) in userMovieAvaliation"
+                    @click="ratedMovie(movie.movieId, rated.value)"
+                    v-for="(rated, i) in userMovieRated"
                     :key="i"
-                    ><v-icon>{{ avaliation.icon }}</v-icon></v-btn
+                    ><v-icon>{{ rated.icon }}</v-icon></v-btn
                   >
                 </v-card-actions>
               </v-card>
@@ -67,7 +108,7 @@ export default {
   data: () => ({
     detail: null,
     genres: null,
-    userMovieAvaliation: [
+    userMovieRated: [
       { icon: "mdi-star", value: 1 },
       { icon: " mdi-star", value: 2 },
       { icon: "mdi-star", value: 3 },
@@ -76,61 +117,94 @@ export default {
     ],
     movies: null,
     moviesByGenre: {},
+    recommendedMovies: [],
+    user: null,
   }),
   watch: {},
+  computed: {},
   created() {
     this.init();
   },
-  mounted() {},
-  computed: {},
   methods: {
     async init() {
       await this.fecthGenres();
       await this.fecthMovies();
       await this.agroupByGenre();
+      this.fetchUser();
+      this.fetchRecommendedMovies();
     },
     agroupByGenre() {
       let result = {};
-      
+
       this.genres.forEach((genre) => {
         Object.defineProperty(result, genre.name, {
-            value: this.movies.filter((movie) => {             
-              return movie.in_genre[0].name === genre.name
-            })
-          });
-        });        
+          value: this.movies.filter((movie) => {
+            return movie.in_genre[0].name === genre.name;
+          }),
+        });
+      });
       this.moviesByGenre = result;
-        
+
       return this.moviesByGenre;
     },
+    fetchUser() {
+      return (this.user = JSON.parse(localStorage.user));
+    },
     async fecthMovies() {
-
       const fetchMovies = await this.$apollo.query({
         query: require(`@/graphql/fetchMovies.gql`),
       });
 
-      return this.movies = fetchMovies.data.Movie;
+      return (this.movies = fetchMovies.data.Movie);
     },
     async fecthGenres() {
-
       const fetchGenres = await this.$apollo.query({
         query: require(`@/graphql/fetchGenres.gql`),
       });
 
       this.genres = fetchGenres.data.Genre;
     },
+    async ratedMovie(movieId, rated) {
+      const movieIsRated = await this.$apollo.query({
+        query: require(`@/graphql/verifyMovieIsRated.gql`),
+        variables: {
+          userId: this.user.userId,
+          movieId: movieId,
+        },
+      });
+      if (!movieIsRated.data.User[0].rated[0]) {
+        return await this.$apollo.mutate({
+          mutation: require(`@/graphql/ratedMovie.gql`),
+          variables: {
+            userId: this.user.userId,
+            movieId: movieId,
+            rated: rated,
+          },
+        });
+      }
+      return await this.$apollo.mutate({
+        mutation: require(`@/graphql/updateRateMovie.gql`),
+        variables: {
+          userId: this.user.userId,
+          movieId: movieId,
+          rated: rated,
+        },
+      });
+    },
+    async fetchRecommendedMovies() {
+      const response = await this.$apollo.query({
+        query: require(`@/graphql/recommendedMovie.gql`),
+        variables: {
+          userId: this.user.userId,
+        },
+      });
+      return (this.recommendedMovies = response.data.User[0].RATED_rel);
+    },
   },
 };
 </script>
-  <!-- exemplo estrutura de dados de um filme 
-  {
-    name:........,
-    genre:.......,
-    actor:.......,
-    cartaz:....,
-  }
-  
-  
-  -->
-  <style>
+<style>
+.bodyWidth {
+  max-width: 1200px;
+}
 </style>
